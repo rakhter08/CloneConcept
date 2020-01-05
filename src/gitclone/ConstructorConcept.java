944510ff7946ea9653cd0ee138e6c43e6331e4e1
rakhter08 <rakhter08@gmail.com>
2020-01-05 16:32:03 -0500

    changed comments

---
+++ src/gitclone/ConstructorConcept.java
@@ -0,0 +1,12 @@
+package gitclone;
+
+public class ConstructorConcept {
+    public void call(){
+        System.out.println("This is my first project.");
+    }
+
+    public static void main(String[] args) {
+        ConstructorConcept c=new ConstructorConcept();
+        c.call();
+    }
+}
