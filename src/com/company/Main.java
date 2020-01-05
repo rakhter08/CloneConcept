a4c517dc671f088b4d2ea053220823871e8e0fb9
rakhter08 <rakhter08@gmail.com>
2020-01-05 15:20:40 -0500

    A method got added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        message();
+    }
+    public static void message(){
+        System.out.println("I'm learing git today.");
     }
 }
